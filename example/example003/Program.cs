bf05be268b6fdde403bac05c8fdde5d5660653b6
Alexey Pigin <116755583+Alexey-Pigin@users.noreply.github.com>
2022-11-21 09:42:36 +0300

    hello5_Summa+Random

--- example/example003/Program.cs
+++ example/example003/Program.cs
@@ -3,6 +3,12 @@ int numberA = 5;
 int numberB = 15;
 Console.WriteLine(numberA+numberB);
 
+int numberE = new Random().Next(1,10);  // 123456789
+Console.WriteLine(numberE);
+int numberF = new Random().Next(1,10);  // 123456789
+Console.WriteLine(numberF);
+Console.WriteLine(numberE+numberF);
+
 int numberC = 3;
 int numberD = 15;
 int result = numberC+numberD;
