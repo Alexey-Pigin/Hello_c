2418881907e25b90b169f8edf918159c567356cb
Alexey Pigin <116755583+Alexey-Pigin@users.noreply.github.com>
2022-11-21 09:11:21 +0300

    hello2

---
+++ example/example002/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите ваше имя");     //  если использовать Console.WriteLine то каждая строка пишется с новой строчки
+string username = Console.ReadLine();  // это не равенство а присвоение, бери то что справа и назови то что слева
+Console.Write("Hello Alex P, ");
+Console.Write(username);               //  если использовать Console.Write то текст и программа бедет работать в 1 строке
